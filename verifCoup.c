a8c29d76189b985603cc745082a3d6a7963de2c2
Maelwennlbdr <101875283+Maelwennlbdr@users.noreply.github.com>
2022-04-20 16:01:47 +0200

    Add files via upload
    ligAlreadyExisting and colAlreadyExisting function

--- verifCoup.c
+++ verifCoup.c
@@ -1,17 +1,53 @@
 #include "verifCoup.h"
 
-bool sameNumberOf0And1InLig(TakuzuGrid takusuGrid, int indexLig){
+bool sameNumberOf0And1InLig(TakuzuGrid takusuGrid, int indexLig) {
     return true;
 }
 
-bool sameNumberOf0And1InCol(TakuzuGrid takusuGrid, int indexCol){
+bool sameNumberOf0And1InCol(TakuzuGrid takusuGrid, int indexCol) {
     return true;
 }
 
-bool ligAlreadyExisting(TakuzuGrid takusuGrid, int indexOfLig){
-    return true;
+bool ligAlreadyExisting(TakuzuGrid takusuGrid, int indexOfLig) {
+    int size, i, j, **grid, counter = 0;
+    grid = takusuGrid.matrice;
+    size = takusuGrid.size;
+    for (i = 0; (i < size) && (counter != size); i++) {
+        if (i != indexOfLig) {
+            for (j = 0; j < size; j++) {
+                if (grid[i][j] == grid[indexOfLig][j]) {
+                    counter++;
+                }
+            }
+            if (counter != size) {
+                counter = 0;
+            }
+        }
+    }
+    if (counter == size) {
+        return true;
+    }
+    return false;
 }
 
-bool colAlreadyExisting(TakuzuGrid takusuGrid, int indexOfCol){
-    return true;
+bool colAlreadyExisting(TakuzuGrid takusuGrid, int indexOfCol) {
+    int size, i, j, **grid, counter = 0;
+    grid = takusuGrid.matrice;
+    size = takusuGrid.size;
+    for (j = 0; (j < size) && (counter != size); j++) {
+        if (j != indexOfCol) {
+            for (i = 0; i < size; i++) {
+                if (grid[i][j] == grid[i][indexOfCol]) {
+                    counter++;
+                }
+            }
+            if (counter != size) {
+                counter = 0;
+            }
+        }
+    }
+    if (counter == size) {
+        return true;
+    }
+    return false;
 }
