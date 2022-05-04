7778b5ee19f80e09563f8a256ed7968e101281d9
RomainDrouet <103487910+RomainDrouet@users.noreply.github.com>
2022-05-04 13:35:32 +0200

    Add files via upload
    je sais pas si ça fonctionne mais c'est beau

--- verifCoup.c
+++ verifCoup.c
@@ -1,53 +1,65 @@
 #include "verifCoup.h"
 
-bool sameNumberOf0And1InLig(TakuzuGrid takusuGrid, int indexLig) {
+
+bool sameNumberOf0And1InLig(TakuzuGrid takusuGrid, int indexLig){
+    int numberOf0=0,numberOf1=0;
+    int**index;
+    index = indexLig;
+    for(int i =0; i<takusuGrid.size; i++){
+        for(int j=0; j<takusuGrid.size; j++){
+            if(index[i][j]==0){
+                numberOf0++;
+            } else{
+                numberOf1++;
+            }
+        }
+        if(numberOf1>numberOf0){
+            printf("il y a plus de 1 que de 0 dans la ligne %d", i);
+            return false;
+        }else if(numberOf0>numberOf1){
+            printf("il y a plus de 0 que de 1 dans la ligne %d", i);
+            return false;
+        }
+        numberOf1=numberOf0=0;
+    }
     return true;
 }
 
-bool sameNumberOf0And1InCol(TakuzuGrid takusuGrid, int indexCol) {
+bool sameNumberOf0And1InCol(TakuzuGrid takusuGrid, int indexCol){
+    int numberOf0=0,numberOf1=0;
+    int**index;
+    index = indexCol;
+    for(int i =0; i<takusuGrid.size; i++){
+        for(int j=0; j<takusuGrid.size; j++){
+            if(index[j][i]==0){
+                numberOf0++;
+            } else{
+                numberOf1++;
+            }
+        }
+        if(numberOf1>numberOf0){
+            printf("il y a plus de 1 que de 0 dans la colonne %d", i);
+            return false;
+        }else if(numberOf0>numberOf1){
+            printf("il y a plus de 0 que de 1 dans la colonne %d", i);
+            return false;
+        }
+        numberOf1=numberOf0=0;
+    }
     return true;
 }
 
-bool ligAlreadyExisting(TakuzuGrid takusuGrid, int indexOfLig) {
-    int size, i, j, **grid, counter = 0;
-    grid = takusuGrid.matrice;
-    size = takusuGrid.size;
-    for (i = 0; (i < size) && (counter != size); i++) {
-        if (i != indexOfLig) {
-            for (j = 0; j < size; j++) {
-                if ((grid[i][j] == grid[indexOfLig][j])&& (grid[i][j] != -1)) {
-                    counter++;
-                }
-            }
-            if (counter != size) {
-                counter = 0;
-            }
-        }
-    }
-    if (counter == size) {
-        return true;
-    }
-    return false;
+bool ligAlreadyExisting(TakuzuGrid takusuGrid, int indexOfLig){
+
+    return true;
 }
 
-bool colAlreadyExisting(TakuzuGrid takusuGrid, int indexOfCol) {
-    int size, i, j, **grid, counter = 0;
-    grid = takusuGrid.matrice;
-    size = takusuGrid.size;
-    for (j = 0; (j < size) && (counter != size); j++) {
-        if (j != indexOfCol) {
-            for (i = 0; i < size; i++) {
-                if ((grid[i][j] == grid[i][indexOfCol]) && (grid[i][j] != -1)){
-                    counter++;
-                }
-            }
-            if (counter != size) {
-                counter = 0;
-            }
-        }
-    }
-    if (counter == size) {
-        return true;
-    }
-    return false;
+bool colAlreadyExisting(TakuzuGrid takusuGrid, int indexOfCol){
+
+    return true;
+}
+
+int validityMove(){
+
+    return 0;
 }
