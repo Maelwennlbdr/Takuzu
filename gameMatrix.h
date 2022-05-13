04ef0718aa9746a30961a41f4d306e1179e66bb8
RomainDrouet <romain.drouet92@gmail.com>
2022-05-13 08:31:59 +0200

    creat 4 new fill with code

---
+++ gameMatrix.h
@@ -0,0 +1,8 @@
+#ifndef TAKUSU_GAMEMATRIX_H
+#define TAKUSU_GAMEMATRIX_H
+#include <stdio.h>
+
+void createGrid4(int**); // Not in master
+void createGrid8(int**); // Not in master
+
+#endif //TAKUSU_GAMEMATRIX_H
